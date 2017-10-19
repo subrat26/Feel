2625d21d410a874e40bf84be8e7a5b9c2e727da8
Subrat Pani <subratpn@gmail.com>
2017-10-20 00:34:44 +0530

    commit

--- app/src/main/java/com/tolkart/feel/Activity/HomeActivity.java
+++ app/src/main/java/com/tolkart/feel/Activity/HomeActivity.java
@@ -1,4 +1,4 @@
-package com.tolkart.feel;
+package com.tolkart.feel.Activity;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -6,6 +6,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.tolkart.feel.R;
 
 public class HomeActivity extends AppCompatActivity {
 
